f3e01b9b29b7d93a2a2d766cfb11a0dadbdbf575
Diana-2305 <Dianamartynova2021@mail.ru>
2023-09-09 16:44:12 +0500

    add: slider

--- js/main.js
+++ js/main.js
@@ -27,8 +27,37 @@ const closeMenu = (event) =>  {
 };  
 window.addEventListener ("scroll", () => {
   this.scrollY > 1 ? lightModeOn(): lightModeOff();
-  });
+});
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
   menu.classList.contains("is-open") ? closeMenu():openMenu();  
-});   
+}); 
+
+const swiper = new Swiper(".swiper", {
+  speed: 400,
+  autoHeight: true,
+  slidesPerView: 1,
+  navigation: {
+    prevEl: ".slider-button-prev",
+    nextEl: ".slider-button-next",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+      
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window widt h is >= 640px
+    1024: {
+      slidesPerView: 4,
+    },
+    // when window widt h is >= 1200px
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+}); 
